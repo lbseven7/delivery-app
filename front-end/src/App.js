4810a433aaddb50a4f34e9c179f3e8e4ac7d22f9
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-10-28 10:15:16 -0300

    Login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <Routes>
+        <Route path="/login" component={ <Login /> } />
+      </Routes>
     </div>
   );
 }
---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="data-login-form-container">
+      <title> LOGIN </title>
+    </div>
+  );
+}
+
+export default Login;
