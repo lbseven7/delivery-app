70d93d6c7d011f0744e8ac04a6c0d1d17a9e8347
Adriano <adrianoo.rdg@gmail.com>
2022-11-03 10:38:26 -0300

    fix: fix de erros de lint

--- back-end/src/helpers/tokenHelper.js
+++ back-end/src/helpers/tokenHelper.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const jwtKey = require("fs")
-  .readFileSync("jwt.evaluation.key", { encoding: "utf-8" });
+const jwtKey = require('fs')
+  .readFileSync('jwt.evaluation.key', { encoding: 'utf-8' });
+
 const JWT_CONFIG = { algorithm: 'HS256', expiresIn: '45min' };
 
 const createToken = (payload) => {
