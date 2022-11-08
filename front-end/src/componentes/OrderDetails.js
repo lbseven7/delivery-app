8a0fb21d8164ac5e646757f9eecd8be5e27fefa5
Adriano <adrianoo.rdg@gmail.com>
2022-11-08 18:11:33 -0300

    feat: adicao da navbar na deta de detalhes

--- front-end/src/componentes/OrderDetails.js
+++ front-end/src/componentes/OrderDetails.js
@@ -2,6 +2,7 @@ import Proptypes from 'prop-types';
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DeliveryContext from '../context/deliveryContext';
+import NavBar from './NavBar';
 
 function OrderDetails() {
   const products = JSON.parse(localStorage.getItem('cartItems'));
@@ -32,91 +33,94 @@ function OrderDetails() {
   };
 
   return (
-    <div onClick={ () => onClick(orderId) } aria-hidden="true">
-      <p
-        data-testid="customer_order_details__element-order-details-label-order-id"
-      >
-        { `Pedido: ${id}` }
-      </p>
+    <>
+      <NavBar />
+      <div onClick={ () => onClick(orderId) } aria-hidden="true">
+        <p
+          data-testid="customer_order_details__element-order-details-label-order-id"
+        >
+          { `Pedido: ${id}` }
+        </p>
 
-      <p
-        data-testid={
-          `customer_order_details__element-order-details-label-delivery-status${id}`
-        }
-      >
-        { status }
-      </p>
-      <button
-        data-testid="customer_order_details__button-delivery-check"
-        type="button"
-        disabled="true"
-      >
-        MARCAR COMO ENTREGUE
-      </button>
-      <p
-        data-testid="customer_order_details__element-order-details-label-order-date"
-      >
-        { orderProductData(saleDate) }
-      </p>
-      <p
-        data-testid="customer_order_details__element-order-details-label-seller-name"
-      >
-        P. Vend: Fulana Pereira
-      </p>
-      <div>
-        {
-          products.map(({ name, quantity, price }, index) => (
-            <div key={ index }>
-              <p
-                data-testid={
-                  `customer_order_details__element-order-table-item-number-${index}`
-                }
-              >
-                Item
-                { index + 1 }
-              </p>
-              <p
-                data-testid={
-                  `customer_order_details__element-order-table-name-${index}`
-                }
-              >
-                Descrição
-                { name }
-              </p>
-              <p
-                data-testid={
-                  `customer_order_details__element-order-table-quantity-${index}`
-                }
-              >
-                Quantidade
-                { quantity }
-              </p>
-              <p
-                data-testid={
-                  `customer_order_details__element-order-table-unit-price-${index}`
-                }
-              >
-                Valor Unitário
-                { price }
-              </p>
-              <p
-                data-testid={
-                  `customer_order_details__element-order-table-sub-total-${index}`
-                }
-              >
-                Sub-total
-                { price * quantity }
-              </p>
-            </div>
-          ))
-        }
+        <p
+          data-testid={
+            `customer_order_details__element-order-details-label-delivery-status${id}`
+          }
+        >
+          { status }
+        </p>
+        <button
+          data-testid="customer_order_details__button-delivery-check"
+          type="button"
+          disabled="true"
+        >
+          MARCAR COMO ENTREGUE
+        </button>
+        <p
+          data-testid="customer_order_details__element-order-details-label-order-date"
+        >
+          { orderProductData(saleDate) }
+        </p>
+        <p
+          data-testid="customer_order_details__element-order-details-label-seller-name"
+        >
+          P. Vend: Fulana Pereira
+        </p>
+        <div>
+          {
+            products.map(({ name, quantity, price }, index) => (
+              <div key={ index }>
+                <p
+                  data-testid={
+                    `customer_order_details__element-order-table-item-number-${index}`
+                  }
+                >
+                  Item
+                  { index + 1 }
+                </p>
+                <p
+                  data-testid={
+                    `customer_order_details__element-order-table-name-${index}`
+                  }
+                >
+                  Descrição
+                  { name }
+                </p>
+                <p
+                  data-testid={
+                    `customer_order_details__element-order-table-quantity-${index}`
+                  }
+                >
+                  Quantidade
+                  { quantity }
+                </p>
+                <p
+                  data-testid={
+                    `customer_order_details__element-order-table-unit-price-${index}`
+                  }
+                >
+                  Valor Unitário
+                  { price }
+                </p>
+                <p
+                  data-testid={
+                    `customer_order_details__element-order-table-sub-total-${index}`
+                  }
+                >
+                  Sub-total
+                  { price * quantity }
+                </p>
+              </div>
+            ))
+          }
+        </div>
+        <p
+          data-testid="customer_order_details__element-order-total-price"
+        >
+          {totalPrice.toFixed(2).replace('.', ',')}
+        </p>
       </div>
-      <p
-        data-testid="customer_order_details__element-order-total-price"
-      >
-        {totalPrice.toFixed(2).replace('.', ',')}
-      </p>
-    </div>
+    </>
   );
 }
 
